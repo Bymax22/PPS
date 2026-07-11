1e8d900a0d60fe80501be8236929698d6bb22ac5
Bymax <kwibisa21@gmail.com>
2026-07-12 01:14:16 +0200

    Fix Student

--- apps/web/app/student/page.tsx
+++ apps/web/app/student/page.tsx
@@ -46,7 +46,7 @@ export default async function StudentDashboardPage() {
         }
       },
       subscriptions: {
-        where: { isActive: true },
+        where: { status: 'ACTIVE' },
         include: {
           plan: {
             include: { program: true }
